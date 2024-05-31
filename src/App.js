798e99d60d8934f7cbeb13770e9dc2c36fe6a7c4
saumyagupta480 <saumyagupta480@gmail.com>
2024-05-31 15:06:40 +0530

    remove navigation bar as it produce eslint warnings

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ let name = "Saumya"
 function App() {
   return (
     <>
-      <nav class="navbar navbar-expand-sm bg-light navbar-light">
-        <div class="container-fluid">
-          <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
-            <li class="nav-item"><a class="nav-link disabled" href="#">Contact</a></li>
-          </ul>
-        </div>
-      </nav>
       <div class="container">
         <h1>hello {name}</h1>
         <p>
